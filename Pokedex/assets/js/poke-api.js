371276ffe287466e990dc946858fb7d36b9cce72
Matheus Baptista <matheushbm192@gmail.com>
2023-05-09 18:39:35 -0300

    Finalizando a Pokedex

--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -2,7 +2,7 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon();
-    pokemon.number = pokeDetail.order;
+    pokemon.number = pokeDetail.id;
     pokemon.name = pokeDetail.name;
 
     const types =  pokeDetail.types.map((typeSlot) => typeSlot.type.name);
@@ -20,9 +20,9 @@ pokeApi.getPokemonDetail = (pokemon) => {
             .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (ofset = 0, limit = 5) => {
+pokeApi.getPokemons = ( offset = 0, limit = 5) => {
 
-    const url = `https://pokeapi.co/api/v2/pokemon?ofset=${ofset}limit=${limit}`;
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
